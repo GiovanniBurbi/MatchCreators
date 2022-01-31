8385a3c88582088c6a5f0ad8de35bbf8fead70cf
Giovanni <giovanniburbi10@gmail.com>
2022-01-31 13:24:25 +0100

    auth page

---
+++ src/components/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-content class=background>
+  </v-content>
+</template>
+<script>
+export default {
+  name: 'Login',
+};
+</script>
+<style scope>
+.background{
+  height: 100%;
+  width: 100%;
+  background-image: linear-gradient(90deg, #673AB7 33.33%,
+  #7E57C2 33.33%, #7E57C2 66.66%, #9575CD 66.66%, #9575CD 99.99%);
+}
+</style>
