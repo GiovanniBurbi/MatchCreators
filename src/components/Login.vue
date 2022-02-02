d3d7d04d4f77fbb1711571d71543f69faaa9c6fc
Giovanni <giovanniburbi10@gmail.com>
2022-02-02 20:48:33 +0100

    registration with transition and date

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,36 +1,119 @@
 <template>
-  <div>
-    <v-card style="margin: 30px auto;" max-width="450" min-height="420" rounded="xl" elevation="10">
-      <v-card-title class="justify-end pr-4 pt-12 pb-4"
-      style="font-size: 30px;font-weight: 300; color: #3F51B5;">
-        Authentication
-      </v-card-title>
-      <v-card-text class="pb-0 pt-2">
-        <v-form >
-          <v-text-field label="Username"
-          clearable prepend-icon="mdi-account">
-          </v-text-field>
-          <v-text-field label="Password"
-          clearable prepend-icon="mdi-lock"></v-text-field>
-        </v-form>
-      </v-card-text>
-      <v-card-actions>
-        <v-btn x-large dark color="indigo" rounded block elevation="2">
-          Login
-        </v-btn>
-      </v-card-actions>
-      <v-card-text class="text-center pt-6 text-body-2">
-        Not registered yet?
-        <v-btn small dark color="indigo" text class="px-1 py-1">
-         Sign up
-        </v-btn>
-      </v-card-text>
-    </v-card>
-  </div>
+  <v-card style="margin: 30px auto;"
+  max-width="450" min-height="380" rounded="xl" elevation="10">
+    <v-window v-model="step">
+      <v-window-item :value="1">
+        <v-card-title class="justify-end pr-4 pt-8 pb-4"
+        style="font-size: 30px;font-weight: 300; color: #3F51B5;">
+          <span>Authentication</span>
+        </v-card-title>
+        <v-card-text class="pb-0 pt-2">
+          <v-form >
+            <v-text-field color="indigo" label="Username"
+            clearable prepend-icon="mdi-account">
+            </v-text-field>
+            <v-text-field color="indigo" label="Password"
+            clearable prepend-icon="mdi-lock"></v-text-field>
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn x-large dark color="indigo" rounded block elevation="2">
+            Login
+          </v-btn>
+        </v-card-actions>
+        <v-card-text class="text-center pt-6 pb-8 text-body-2">
+          Not registered yet?
+          <v-btn small dark color="indigo" text class="px-1 py-1" @click="step++">
+          Sign up
+          </v-btn>
+        </v-card-text>
+      </v-window-item>
+
+      <v-window-item :value="2">
+        <v-card-title class="justify-start pr-4 pt-8 pb-4"
+        style="font-size: 30px;font-weight: 300; color: #3F51B5;">
+          <span>Registration</span>
+        </v-card-title>
+        <v-card-text class="pb-0 pt-2">
+          <v-form >
+            <v-text-field color="indigo" label="Username"
+            clearable prepend-icon="mdi-account">
+            </v-text-field>
+            <v-text-field color="indigo" label="Email"
+            clearable prepend-icon="mdi-email">
+            </v-text-field>
+            <v-text-field color="indigo" label="Password"
+            clearable prepend-icon="mdi-lock"></v-text-field>
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn @click="step++" x-large dark color="indigo" rounded block elevation="2">
+            Sign up
+          </v-btn>
+        </v-card-actions>
+        <v-card-text class="text-center pt-6 pb-8 text-body-2">
+          Already have an account?
+          <v-btn small dark color="indigo" text class="px-1 py-1" @click="step--">
+            login
+          </v-btn>
+        </v-card-text>
+      </v-window-item>
+
+      <v-window-item :value="3">
+        <v-card-title class="justify-start pr-4 pt-8 pb-4"
+        style="font-size: 30px;font-weight: 300; color: #3F51B5;">
+          <span>Complete your profile</span>
+        </v-card-title>
+        <v-card-text class="pb-0 pt-4">
+          <v-form >
+            <v-menu ref="menu" v-model="menu" :close-on-content-click="false"
+            transition="scale-transition" offset-y min-width="auto">
+              <template v-slot:activator="{ on, attrs }">
+                <v-text-field v-model="date" label="Birth date"
+                prepend-icon="mdi-calendar" color="indigo"
+                readonly clearable v-bind="attrs" v-on="on">
+                </v-text-field>
+              </template>
+              <v-date-picker v-model="date" :active-picker.sync="activePicker"
+              :max="(new Date(Date.now() - (new Date()).
+              getTimezoneOffset() * 60000)).toISOString().substr(0, 10)"
+              min="1950-01-01" @change="save"
+              color="indigo">
+              </v-date-picker>
+            </v-menu>
+          </v-form>
+        </v-card-text>
+        <v-card-actions class="pt-8">
+          <v-btn x-large dark color="indigo" rounded block elevation="2">
+            Let's start!
+          </v-btn>
+        </v-card-actions>
+      </v-window-item>
+    </v-window>
+  </v-card>
 </template>
 
 <script>
 export default {
   name: 'Login',
+
+  data() {
+    return {
+      step: 1,
+      activePicker: null,
+      date: null,
+      menu: false,
+    };
+  },
+  watch: {
+    menu(val) {
+      return val && setTimeout(() => { this.activePicker = 'YEAR'; });
+    },
+  },
+  methods: {
+    save(date) {
+      this.$refs.menu.save(date);
+    },
+  },
 };
 </script>
