9be2a5f90c88556e810e750a3ddbb4c00321df5d
Giovanni <giovanniburbi10@gmail.com>
2022-02-10 16:52:17 +0100

    added font awesome icons

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import '@fortawesome/fontawesome-free/css/all.css';
 import PositionIcon from '../icons/PositionIcon.vue';
 import BallIcon from '../icons/BallIcon.vue';
 import PlayerIcon from '../icons/PlayerIcon.vue';
@@ -19,5 +20,6 @@ export default new Vuetify({
         component: PlayerIcon,
       },
     },
+    iconfont: 'mdi' || 'fa',
   },
 });
