c1d33840aac19228f6b05fd8948b48e30bf14f14
Giovanni <giovanniburbi10@gmail.com>
2022-02-11 15:39:03 +0100

    cleaner, separated in other component

--- src/components/FinderHeader.vue
+++ src/components/FinderHeader.vue
@@ -1,117 +1,70 @@
 <template>
   <div>
     <v-row justify="space-between" align="center">
+
       <h1 :class="['white--text font-weight-bold header']">
         Search a Match
       </h1>
+
       <v-btn
       :small="xsOnly"
-      :color="showFilters ?  'white' : 'indigo'"
+      :color="showFilters ?  'white' : 'deep-purple'"
       rounded
       @click="showFilters = !showFilters"
       >
         <span
-        :class="['hidden-xs-only', showFilters ? 'indigo--text' : 'white--text']">Filters</span>
+        :class="['hidden-xs-only',
+        showFilters ? 'deep-purple--text' : 'white--text']">Filters</span>
         <v-icon
         :small="xsOnly"
         :right="smAndUp"
-        :class="showFilters ? 'icon-indigo' : 'icon-white'"
+        :color="showFilters ? 'deep-purple' : 'white'"
         >
           fa-solid fa-sliders
         </v-icon>
       </v-btn>
     </v-row>
+
     <v-row>
       <v-scroll-y-transition hide-on-leave>
+
         <v-container v-if="showFilters" fluid class="filtersGroup">
+
           <v-row justify="center">
+
             <v-col cols="3">
-              <v-sheet color="transparent" class="d-flex justify-center">
-                <v-btn dark color="deep-purple">
-                  <v-icon :left="smAndUp" size="22">mdi-map-marker-outline</v-icon>
-                  <span class="hidden-xs-only">Location</span>
-                </v-btn>
-              </v-sheet>
+              <match-filter category="location"/>
             </v-col>
+
             <v-col cols="3">
-              <v-sheet color="transparent" class="d-flex justify-center">
-                <v-btn dark color="deep-purple">
-                  <v-icon size="22" :left="smAndUp">mdi-calendar</v-icon>
-                  <span class="hidden-xs-only">Date</span>
-                </v-btn>
-              </v-sheet>
+              <match-filter category="date"/>
             </v-col>
+
             <v-col cols="3">
-              <v-sheet color="transparent" class="d-flex justify-center">
-                <v-btn dark color="deep-purple">
-                  <v-icon size="22" :left="smAndUp">mdi-clock-outline</v-icon>
-                  <span class="hidden-xs-only">Time</span>
-                </v-btn>
-              </v-sheet>
+              <match-filter category="time"/>
             </v-col>
+
             <v-col cols="3">
-              <v-sheet color="transparent" class="d-flex justify-center">
-                <v-btn dark color="deep-purple">
-                  <v-icon size=24 class="icon-white" :left="smAndUp">$position-icon</v-icon>
-                  <span class="hidden-xs-only">position</span>
-                </v-btn>
-              </v-sheet>
+              <match-filter category="position"/>
             </v-col>
+
           </v-row>
+          <div>
+
+          </div>
+
         </v-container>
       </v-scroll-y-transition>
     </v-row>
     <v-row justify="center">
-      <v-chip
-        class="ma-2"
-        close
-        color="indigo"
-        text-color="white"
-      >
-        <v-icon left size="22">
-          mdi-clock-outline
-        </v-icon>
-        10:30 - 12:30
-      </v-chip>
-      <v-chip
-        class="ma-2"
-        close
-        color="indigo"
-        text-color="white"
-        @click:close="closeChip"
-      >
-        <v-icon left size="22">
-          mdi-calendar
-        </v-icon>
-        29 Nov - 11 Dec
-      </v-chip>
-      <v-chip
-        class="ma-2"
-        close
-        color="indigo"
-        text-color="white"
-      >
-        <v-icon class="icon-white" left>
-          $position-icon
-        </v-icon>
-        Forward
-      </v-chip>
-      <v-chip
-        class="ma-2"
-        close
-        color="indigo"
-        text-color="white"
-      >
-        <v-icon size="22" left>
-          mdi-map-marker-outline
-        </v-icon>
-        Rose Stadium
-      </v-chip>
+      <!-- <filter-chips-group/> -->
     </v-row>
   </div>
 </template>
 
 <script>
+/* import FilterChipsGroup from './FilterChipsGroup.vue'; */
+import MatchFilter from './MatchFilter.vue';
 import BreakpointsCond from '../mixins/BreakpointsCond';
 
 export default {
@@ -121,6 +74,11 @@ export default {
     return {
       showFilters: false,
     };
+  },
+
+  components: {
+    /* FilterChipsGroup, */
+    MatchFilter,
   },
 
   methods: {
@@ -141,13 +99,5 @@ export default {
 }
 .filtersGroup {
   max-width: 1200px;
-}
-.icon-white {
-  /* white */
-  filter: invert(99%) sepia(3%) saturate(1032%) hue-rotate(291deg) brightness(122%) contrast(100%);
-}
-.icon-indigo {
-  filter: invert(26%) sepia(55%) saturate(2295%)
-  hue-rotate(217deg) brightness(90%) contrast(83%);
 }
 </style>
