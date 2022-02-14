de0981da21b1c29ae7358bbf8574a7cbedf4d67d
Giovanni <giovanniburbi10@gmail.com>
2022-02-14 12:18:31 +0100

    Cards group fetch db, skeleton on waiting

--- src/components/MatchCardsGroup.vue
+++ src/components/MatchCardsGroup.vue
@@ -1,6 +1,18 @@
 <template>
   <v-container fluid>
-    <v-row justify="center">
+    <v-row justify="center" v-if="!loaded">
+      <v-skeleton-loader
+      class="mx-2 my-2"
+      v-for="index in 12"
+      :key="index"
+      elevation="8"
+      tile
+      type="card"
+      width="340"
+      height="200"
+      />
+    </v-row>
+    <v-row justify="center" v-else>
       <match-card
       v-for="match in matches"
       :key="match.id"
@@ -11,6 +23,7 @@
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex';
 import MatchCard from './MatchCard.vue';
 
 export default {
@@ -18,196 +31,52 @@ export default {
 
   data() {
     return {
-      matches: [
-        {
-          id: 1,
-          date: '2022-03-01',
-          startTime: '10:00',
-          endTime: '12:00',
-          location: 'Albereta',
-          positions: {
-            goalkeepers: 2,
-            defenders: 3,
-            forwards: 4,
-          },
-        },
-        {
-          id: 2,
-          date: '2022-08-22',
-          startTime: '18:00',
-          endTime: '19:00',
-          location: 'Galluzzo',
-          positions: {
-            goalkeepers: 1,
-            defenders: 4,
-            forwards: 4,
-          },
-        },
-        {
-          id: 3,
-          date: '2022-04-17',
-          startTime: '11:00',
-          endTime: '12:00',
-          location: 'Firenze sud',
-          positions: {
-            goalkeepers: 1,
-            defenders: 2,
-            forwards: 4,
-          },
-        },
-        {
-          id: 4,
-          date: '2022-07-11',
-          startTime: '18:00',
-          endTime: '21:00',
-          location: 'Albereta',
-          positions: {
-            goalkeepers: 1,
-            defenders: 2,
-            forwards: 3,
-          },
-        },
-        {
-          id: 5,
-          date: '2022-03-11',
-          startTime: '14:00',
-          endTime: '15:00',
-          location: 'Legnaia',
-          positions: {
-            goalkeepers: 2,
-            defenders: 4,
-            forwards: 3,
-          },
-        },
-        {
-          id: 6,
-          date: '2022-03-21',
-          startTime: '17:00',
-          endTime: '18:00',
-          location: 'Due strade',
-          positions: {
-            goalkeepers: 2,
-            defenders: 2,
-            forwards: 3,
-          },
-        },
-        {
-          id: 7,
-          date: '2022-04-01',
-          startTime: '15:00',
-          endTime: '16:00',
-          location: 'Porta Romana',
-          positions: {
-            goalkeepers: 1,
-            defenders: 2,
-            forwards: 4,
-          },
-        },
-        {
-          id: 8,
-          date: '2022-05-05',
-          startTime: '18:00',
-          endTime: '19:00',
-          location: 'Scandicci',
-          positions: {
-            goalkeepers: 1,
-            defenders: 4,
-            forwards: 3,
-          },
-        },
-        {
-          id: 9,
-          date: '2022-04-14',
-          startTime: '10:00',
-          endTime: '11:00',
-          location: 'Albereta',
-          positions: {
-            goalkeepers: 2,
-            defenders: 2,
-            forwards: 4,
-          },
-        },
-        {
-          id: 10,
-          date: '2022-05-11',
-          startTime: '11:00',
-          endTime: '12:00',
-          location: 'Campo di Marte',
-          positions: {
-            goalkeepers: 1,
-            defenders: 4,
-            forwards: 3,
-          },
-        },
-        {
-          id: 11,
-          date: '2022-04-28',
-          startTime: '13:00',
-          endTime: '15:00',
-          location: 'Settignano',
-          positions: {
-            goalkeepers: 0,
-            defenders: 4,
-            forwards: 4,
-          },
-        },
-        {
-          id: 12,
-          date: '2022-03-01',
-          startTime: '12:00',
-          endTime: '14:00',
-          location: 'Albereta',
-          positions: {
-            goalkeepers: 1,
-            defenders: 0,
-            forwards: 4,
-          },
-        },
-        {
-          id: 13,
-          date: '2022-03-10',
-          startTime: '9:00',
-          endTime: '10:00',
-          location: 'Ponte a sieve',
-          positions: {
-            goalkeepers: 2,
-            defenders: 4,
-            forwards: 3,
-          },
-        },
-        {
-          id: 14,
-          date: '2022-03-05',
-          startTime: '10:00',
-          endTime: '11:00',
-          location: 'Mercatale',
-          positions: {
-            goalkeepers: 0,
-            defenders: 3,
-            forwards: 4,
-          },
-        },
-        {
-          id: 15,
-          date: '2022-07-11',
-          startTime: '18:00',
-          endTime: '21:00',
-          location: 'Albereta',
-          positions: {
-            goalkeepers: 1,
-            defenders: 2,
-            forwards: 3,
-          },
-        },
-      ],
+      matches: [],
+      loaded: null,
     };
   },
 
   components: {
     MatchCard,
   },
+
+  methods: {
+    ...mapActions({ fetchMatches: 'matches/allMatches' }),
+  },
+
+  computed: {
+    ...mapGetters({ getAllMatches: 'matches/getMatches' }),
+    ...mapGetters({ getStatusMatches: 'matches/getStatusMatches' }),
+  },
+
+  watch: {
+    getAllMatches(newVal, oldVal) {
+      if (oldVal.length === 0) {
+        if (newVal.length !== 0) {
+          this.matches = newVal;
+        }
+      }
+    },
+    getStatusMatches(newVal) {
+      this.loaded = newVal;
+    },
+  },
+
+  created() {
+    this.loaded = this.getStatusMatches;
+    if (!this.loaded) {
+      this.fetchMatches();
+    } else {
+      this.matches = this.getAllMatches;
+    }
+  },
+
 };
 </script>
 
 <style scoped>
+.background-skeleton {
+  background-image: linear-gradient(to top, #c5cae9, #c5cae9, #c5cae9, #c5cae9, #c5cae9,
+  #c8cdea, #cbcfeb, #ced2ec, #d4d8ef, #dbdef1, #e1e4f4, #e8eaf6);
+}
 </style>
