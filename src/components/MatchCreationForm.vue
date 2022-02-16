5b377e5fefbb9f9aee6de7905ccfbd4d344e8ccc
Giovanni <giovanniburbi10@gmail.com>
2022-02-17 00:43:54 +0100

    some decoration

--- src/components/MatchCreationForm.vue
+++ src/components/MatchCreationForm.vue
@@ -1,33 +1,33 @@
 <template>
-  <v-container fluid class="form">
+  <v-container fluid>
 
-    <div class="bg-field d-inline-flex mb-10">
+    <div class="bg-field d-inline-flex mb-12">
 
-      <div class="background medium d-inline-flex justify-center align-center">
-        <h1 class="text-button white--text pr-1">Date</h1>
-        <v-icon size="20" dark>mdi-calendar</v-icon>
+      <div class="background-label d-inline-flex px-3 justify-center align-center">
+        <h1 class="text-button white--text label-text pr-1">Date</h1>
+        <v-icon size="20" dark class="label-text">mdi-calendar</v-icon>
       </div>
 
       <v-text-field solo flat label="Define the date"></v-text-field>
 
     </div>
 
-    <div class="bg-field d-inline-flex mb-10">
+    <div class="bg-field d-inline-flex mb-12">
 
-      <div class="background medium d-inline-flex justify-center align-center">
-        <h1 class="text-button white--text pr-1">Time</h1>
-        <v-icon size="20" dark>mdi-clock-outline</v-icon>
+      <div class="background-label d-inline-flex px-3 justify-center align-center">
+        <h1 class="text-button white--text label-text pr-1">Time</h1>
+        <v-icon size="20" dark class="label-text">mdi-clock-outline</v-icon>
       </div>
 
       <v-text-field solo flat class="d-inline-flex" label="Define the time"></v-text-field>
 
     </div>
 
-    <div class="bg-field-large d-inline-flex mb-10 mr-5">
+    <div class="bg-field d-inline-flex mb-12">
 
-      <div class="background large d-inline-flex justify-center align-center">
-        <h1 class="text-button white--text pr-1">Location</h1>
-        <v-icon size="20" dark>mdi-map-marker-outline</v-icon>
+      <div class="background-label d-inline-flex px-2 justify-center align-center">
+        <h1 class="text-button white--text label-text pr-1">Location</h1>
+        <v-icon size="20" dark class="label-text">mdi-map-marker-outline</v-icon>
       </div>
 
       <v-text-field solo
@@ -44,37 +44,27 @@ export default {
 </script>
 
 <style scoped>
-.background {
-  background-color: #512DA8;
-  height: 49px;
-  border-radius: 5px 0 0 5px;
-}
-.medium {
-  width: 100px
-}
-.large {
-  width: 122px;
-}
-.bg-field {
-  background-color: white;
-  width: 300px;
-  height: 49px;
-  border-radius: 6px;
-}
-.bg-field-large {
-  background-color: white;
-  width: 326px;
-  height: 49px;
-  border-radius: 6px;
-}
 .container {
-  height: 70vh;
-}
-.form {
+  height: 54vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding-bottom: 200px;
+}
+.background-label {
+  background-color: #512DA8;
+  height: 49px;
+  border-radius: 5px 0 0 5px;
+  width: fit-content;
+}
+.bg-field {
+  background-color: white;
+  width: calc(260px + 14vw);
+  height: 49px;
+  border-radius: 6px;
+  box-shadow: 2px 2px 12px rgba(0, 0, 0, 0.6);
+}
+.label-text {
+  text-shadow: 2px 2px 3px black;
 }
 </style>
