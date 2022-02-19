f614e0e69dae48c7b6ed4739864d4a305de43b34
Giovanni <giovanniburbi10@gmail.com>
2022-02-19 16:10:18 +0100

    minor change

--- src/components/DetailsRecap.vue
+++ src/components/DetailsRecap.vue
@@ -7,7 +7,7 @@
           <v-icon size="20" dark class="label-text py-2">mdi-calendar</v-icon>
         </div>
         <div class="bg-field d-inline-flex">
-          <h1 class="text-subtitle-1 pl-1 py-1">27th September 2022</h1>
+          <h1 class="text-subtitle-1 pl-1 py-1">27th Sept 2022</h1>
         </div>
       </div>
 
@@ -52,7 +52,7 @@ export default {
 }
 .bg-field {
   background-color: white;
-  width: 180px;
+  width: 140px;
   white-space: nowrap;
   border-radius: 0 6px 6px 0;
   box-shadow: 2px 2px 12px rgba(0, 0, 0, 0.6);
